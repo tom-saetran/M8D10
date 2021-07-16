d4e72b8f5b50d96adbca208f5657d86d121dbcf0
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-07-16 20:25:03 +0200

    0 type errors anymore :eyes:

--- src/auth/tools.ts
+++ src/auth/tools.ts
@@ -1,37 +1,37 @@
 import jwt from "jsonwebtoken"
-import { User } from "../interfaces"
-import UserModel from "../services/users/schema"
+import UserModel, { User } from "../services/users/schema"
 
-export const JWTAuthenticate = async user => {
-    const accessToken = await generateJWT({ _id: user._id })
-    const refreshToken = await generateRefreshJWT({ _id: user._id })
+const { JWT_SECRET, JWT_REFRESH_SECRET } = process.env
+if (!JWT_SECRET || !JWT_REFRESH_SECRET) throw new Error("Environment variables unreachable.")
+
+export const JWTAuthenticate = async (user: User) => {
+    const accessToken = (await generateJWT({ _id: user._id })) as string
+    const refreshToken = (await generateRefreshJWT({ _id: user._id })) as string
     user.refreshToken = refreshToken
     await user.save()
     return { accessToken, refreshToken }
 }
 
-const generateJWT = payload =>
-    new Promise((resolve, reject) => jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "15m" }, (err, token) => (err ? reject(err) : resolve(token))))
+const generateJWT = (payload: object) =>
+    new Promise((resolve, reject) => jwt.sign(payload, JWT_SECRET, { expiresIn: "15m" }, (err, token) => (err ? reject(err) : resolve(token))))
 
-export const verifyToken = token =>
-    new Promise((resolve, reject) => jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => (err ? reject(err) : resolve(decodedToken))))
+const generateRefreshJWT = (payload: object) =>
+    new Promise((resolve, reject) => jwt.sign(payload, JWT_REFRESH_SECRET, { expiresIn: "1 week" }, (err, token) => (err ? reject(err) : resolve(token))))
 
-const generateRefreshJWT = payload =>
-    new Promise((resolve, reject) =>
-        jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: "1 week" }, (err, token) => (err ? reject(err) : resolve(token)))
-    )
+export const verifyToken = (token: string) =>
+    new Promise((resolve, reject) => jwt.verify(token, JWT_SECRET, (err, decodedToken) => (err ? reject(err) : resolve(decodedToken))))
 
-const verifyRefreshToken = token =>
-    new Promise((resolve, reject) => jwt.verify(token, process.env.JWT_REFRESH_SECRET, (err, decodedToken) => (err ? reject(err) : resolve(decodedToken))))
+const verifyRefreshToken = (token: string) =>
+    new Promise((resolve, reject) => jwt.verify(token, JWT_REFRESH_SECRET, (err, decodedToken) => (err ? reject(err) : resolve(decodedToken))))
 
-export const refreshTokens = async actualRefreshToken => {
-    const content = await verifyRefreshToken(actualRefreshToken)
+export const refreshTokens = async (actualRefreshToken: string) => {
+    const content = (await verifyRefreshToken(actualRefreshToken)) as User
     const user = await UserModel.findById(content._id)
 
     if (!user) throw new Error("User not found")
     if (user.refreshToken === actualRefreshToken) {
-        const newAccessToken = await generateJWT({ _id: user._id })
-        const newRefreshToken = await generateRefreshJWT({ _id: user._id })
+        const newAccessToken = (await generateJWT({ _id: user._id })) as string
+        const newRefreshToken = (await generateRefreshJWT({ _id: user._id })) as string
         user.refreshToken = newRefreshToken
         await user.save()
         return { newAccessToken, newRefreshToken }
